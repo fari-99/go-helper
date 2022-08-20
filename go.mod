8909a284b24022701ddedfb65b7e7a1884d31952
Fadhlan Rizal <fadhlan.fari@gmail.com>
2022-08-20 22:19:18 +0700

    adding generate password and adding aditional time

--- go.mod
+++ go.mod
@@ -3,3 +3,8 @@ module github.com/fari-99/go-helper
 go 1.18
 
 require golang.org/x/exp v0.0.0-20220722155223-a9213eeb770e
+
+require (
+	github.com/nbutton23/zxcvbn-go v0.0.0-20210217022336-fa2cb2858354 // indirect
+	golang.org/x/crypto v0.0.0-20220817201139-bc19a97f63c8 // indirect
+)
--- utilities.go
+++ utilities.go
@@ -3,7 +3,10 @@ package gohelper
 import (
 	"fmt"
 	"reflect"
+	"time"
 
+	"github.com/nbutton23/zxcvbn-go"
+	"golang.org/x/crypto/bcrypt"
 	"golang.org/x/exp/slices"
 )
 
@@ -47,4 +50,58 @@ func InArray(needle any, haystack []any) (bool, int, error) {
 	}
 
 	return false, -1, nil
+}
+
+type Passwords struct {
+	Email    string
+	Username string
+	Password string
+}
+
+// GeneratePassword generates bcrypt hash string of the given plaintext password
+func GeneratePassword(password Passwords, passwordCost int8) (hash *string, err error) {
+	userInput := []string{password.Username, password.Email}
+	secret := password.Password
+
+	checkPassword := zxcvbn.PasswordStrength(secret, userInput)
+	if checkPassword.Score <= 2 {
+		return nil, fmt.Errorf("your password not good enough, please try again")
+	}
+
+	hashedPasswordByte, err := bcrypt.GenerateFromPassword([]byte(secret), int(passwordCost))
+	if err != nil {
+		return nil, err
+	}
+
+	hashedPassword := string(hashedPasswordByte)
+	return &hashedPassword, nil
+}
+
+const timeSeconds = "seconds"
+const timeMinutes = "minutes"
+const timeHours = "hours"
+const timeDays = "days"
+const timeMonths = "months"
+const timeYears = "years"
+
+// AddTime adding additional time to your time.
+func AddTime(yourTime time.Time, addedTime int64, timeType string) (resultTime time.Time, err error) {
+	switch timeType {
+	case timeSeconds:
+		resultTime = yourTime.Add(time.Second * time.Duration(addedTime))
+	case timeMinutes:
+		resultTime = yourTime.Add(time.Minute * time.Duration(addedTime))
+	case timeHours:
+		resultTime = yourTime.Add(time.Hour * time.Duration(addedTime))
+	case timeDays:
+		resultTime = yourTime.AddDate(0, 0, int(addedTime))
+	case timeMonths:
+		resultTime = yourTime.AddDate(0, int(addedTime), 0)
+	case timeYears:
+		resultTime = yourTime.AddDate(int(addedTime), 0, 0)
+	default:
+		err = fmt.Errorf("time type is not supported, please pick (seconds, minutes, hours, days, months, years)")
+	}
+
+	return resultTime, err
 }
